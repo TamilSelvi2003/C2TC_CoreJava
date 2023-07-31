bffed1bd02d078d0718c34fdd11787f346d2cf7d
Louinazeefransco <Louinazeefransco@Louinazeefransco>
2023-07-31 15:02:55 +0530

    First commit

---
+++ src/com/cg/dayone/Identifier.java
@@ -0,0 +1,11 @@
+package com.cg.dayone;
+
+public class Identifier {
+	public static void main(String[] args) {
+		String name="Tamil";
+		int age=20;
+		int id=23;
+		System.out.println(name+" "+ age +" "+ id+" ");
+
+	}
+}
